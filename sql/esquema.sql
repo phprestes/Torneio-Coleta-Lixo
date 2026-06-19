474ac15bb2275937081a7338b6f3d9cdf644e7b2
Pedro H. S. Prestes <pedrohsprestes@gmail.com>
2026-06-19 12:50:52 -0300

    Refactor SQL schema: update data types, add constraints, and improve table definitions

--- sql/esquema.sql
+++ sql/esquema.sql
@@ -1,61 +1,71 @@
--- Tabelas sem FK
+-- DOMÍNIOS
+CREATE DOMAIN SIGLA_ALFA_3 AS CHAR(3)
+CHECK (VALUE ~ '^[A-Z]{3}$');
 
+-- TABELAS SEM CHAVE ESTRANGEIRA (FK)
 CREATE TABLE Torneio (
-    Ano NUMBER NOT NULL,
-    Tema VARCHAR2(30),
-    Porcentagem_Regional NUMBER NOT NULL,
-    Porcentagem_Nacional NUMBER NOT NULL,
-    Porcentagem_Continental NUMBER NOT NULL,
-    Porcentagem_Internacional NUMBER NOT NULL,
+    Ano INTEGER NOT NULL,
+    Tema VARCHAR(30),
+    Porcentagem_Regional NUMERIC NOT NULL,
+    Porcentagem_Nacional NUMERIC NOT NULL,
+    Porcentagem_Continental NUMERIC NOT NULL,
+    Porcentagem_Internacional NUMERIC NOT NULL,
     
-    CONSTRAINT PK_Torneio PRIMARY KEY (Ano)
+    CONSTRAINT PK_Torneio PRIMARY KEY (Ano),
+    CONSTRAINT CK_Porcentagem CHECK(
+        (Porcentagem_Regional BETWEEN 0 AND 100) AND 
+        (Porcentagem_Nacional BETWEEN 0 AND 100) AND
+        (Porcentagem_Continental BETWEEN 0 AND 100) AND
+        (Porcentagem_Internacional BETWEEN 0 AND 100)
+    ),
+    CONSTRAINT CK_Ano CHECK (Ano >= 1900)
 );
 
 CREATE TABLE Escola (
-    ID NUMBER NOT NULL,
-    Tipo_Documento VARCHAR2(10) NOT NULL,
-    Numero_Documento VARCHAR2(20) NOT NULL,
-    Sigla_Pais CHAR(2) NOT NULL,
-    Nome VARCHAR2(255) NOT NULL,
+    ID INTEGER NOT NULL,
+    Tipo_Documento VARCHAR(10) NOT NULL,
+    Numero_Documento VARCHAR(20) NOT NULL,
+    Sigla_Pais SIGLA_ALFA_3 NOT NULL,
+    Nome VARCHAR(255) NOT NULL,
     
     CONSTRAINT PK_Escola PRIMARY KEY (ID),
     CONSTRAINT UK_Escola UNIQUE (Tipo_Documento, Numero_Documento, Sigla_Pais)
 );
 
 CREATE TABLE Lixo (
-    Categoria VARCHAR2(30) NOT NULL,
-    Pontuacao_KG NUMBER NOT NULL,
+    Categoria VARCHAR(30) NOT NULL,
+    Pontuacao_KG NUMERIC NOT NULL,
     
-    CONSTRAINT PK_Lixo PRIMARY KEY (Categoria)
+    CONSTRAINT PK_Lixo PRIMARY KEY (Categoria),
+    CONSTRAINT CK_Pontuacao_Lixo CHECK (Pontuacao_KG >= 0)
 );
 
 CREATE TABLE Transportadora (
-    ID NUMBER NOT NULL,
-    Tipo_Documento VARCHAR2(10) NOT NULL,
-    Numero_Documento VARCHAR2(20) NOT NULL,
-    Sigla_Pais CHAR(2) NOT NULL,
-    Nome VARCHAR2(255),
+    ID INTEGER NOT NULL,
+    Tipo_Documento VARCHAR(10) NOT NULL,
+    Numero_Documento VARCHAR(20) NOT NULL,
+    Sigla_Pais SIGLA_ALFA_3 NOT NULL,
+    Nome VARCHAR(255),
     
     CONSTRAINT PK_Transportadora PRIMARY KEY (ID),
     CONSTRAINT UK_Transportadora UNIQUE (Tipo_Documento, Numero_Documento, Sigla_Pais)
 );
 
 CREATE TABLE Centro_de_Reciclagem (
-    ID NUMBER NOT NULL,
-    Tipo_Documento VARCHAR2(10) NOT NULL,
-    Numero_Documento VARCHAR2(20) NOT NULL,
-    Sigla_Pais CHAR(2) NOT NULL,
-    Nome VARCHAR2(255),
+    ID INTEGER NOT NULL,
+    Tipo_Documento VARCHAR(10) NOT NULL,
+    Numero_Documento VARCHAR(20) NOT NULL,
+    Sigla_Pais SIGLA_ALFA_3 NOT NULL,
+    Nome VARCHAR(255),
     
     CONSTRAINT PK_Centro_de_Reciclagem PRIMARY KEY (ID),
     CONSTRAINT UK_Centro_de_Reciclagem UNIQUE(Tipo_Documento, Numero_Documento, Sigla_Pais)
 );
 
--- Tabelas com FK
-
+-- TABELAS COM CHAVE ESTRANGEIRA (FK)
 CREATE TABLE Patrocinadores_Torneio (
-    Torneio NUMBER NOT NULL,
-    Patrocinador VARCHAR2(255) NOT NULL,
+    Torneio INTEGER NOT NULL,
+    Patrocinador VARCHAR(255) NOT NULL,
     
     CONSTRAINT PK_Patrocinadores PRIMARY KEY (Torneio, Patrocinador),
     CONSTRAINT FK_Patrocinadores FOREIGN KEY (Torneio)
@@ -64,26 +74,27 @@ CREATE TABLE Patrocinadores_Torneio (
 );
 
 CREATE TABLE Tutor (
-    ID NUMBER NOT NULL,
-    Tipo_Documento VARCHAR2(10) NOT NULL,
-    Numero_Documento VARCHAR2(20) NOT NULL,
-    Sigla_Pais CHAR(2) NOT NULL,
-    Nome VARCHAR2(255) NOT NULL,
-    Contato NUMBER NOT NULL,
-    Cargo VARCHAR2(30),
-    Escola NUMBER NOT NULL,
+    ID INTEGER NOT NULL,
+    Tipo_Documento VARCHAR(10) NOT NULL,
+    Numero_Documento VARCHAR(20) NOT NULL,
+    Sigla_Pais SIGLA_ALFA_3 NOT NULL,
+    Nome VARCHAR(255) NOT NULL,
+    Contato VARCHAR(15) NOT NULL,
+    Cargo VARCHAR(30),
+    Escola INTEGER NOT NULL,
     
     CONSTRAINT PK_Tutor PRIMARY KEY (ID),
     CONSTRAINT UK_Tutor UNIQUE(Tipo_Documento, Numero_Documento, Sigla_Pais),
     CONSTRAINT FK_Tutor FOREIGN KEY (Escola)
         REFERENCES Escola(ID)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT CK_Tutor_Contato CHECK (Contato ~ '^\+?[0-9]{8,15}$') 
 );
 
 CREATE TABLE Equipe (
-    Nome VARCHAR2(255) NOT NULL,
-    Ano NUMBER NOT NULL,
-    Tutor NUMBER NOT NULL,
+    Nome VARCHAR(255) NOT NULL,
+    Ano INTEGER NOT NULL,
+    Tutor INTEGER NOT NULL,
     
     CONSTRAINT PK_Equipe PRIMARY KEY (Nome, Ano),
     CONSTRAINT FK_Equipe FOREIGN KEY (Tutor)
@@ -92,18 +103,18 @@ CREATE TABLE Equipe (
 );
 
 CREATE TABLE Aluno (
-    ID NUMBER NOT NULL,
-    Tipo_Documento VARCHAR2(10) NOT NULL,
-    Numero_Documento VARCHAR2(20) NOT NULL,
-    Sigla_Pais CHAR(2) NOT NULL,
-    Nome VARCHAR2(255) NOT NULL,
-    Contato NUMBER,
-    Serie NUMBER NOT NULL,
-    Nome_Responsavel VARCHAR2(255),
-    Contato_Responsavel NUMBER,
-    Escola NUMBER NOT NULL,
-    Nome_Equipe VARCHAR2(255) NOT NULL,
-    Ano_Equipe NUMBER NOT NULL,
+    ID INTEGER NOT NULL,
+    Tipo_Documento VARCHAR(10) NOT NULL,
+    Numero_Documento VARCHAR(20) NOT NULL,
+    Sigla_Pais SIGLA_ALFA_3 NOT NULL,
+    Nome VARCHAR(255) NOT NULL,
+    Contato VARCHAR(15), 
+    Serie INTEGER NOT NULL,
+    Nome_Responsavel VARCHAR(255),
+    Contato_Responsavel VARCHAR(15), 
+    Escola INTEGER NOT NULL,
+    Nome_Equipe VARCHAR(255) NOT NULL,
+    Ano_Equipe INTEGER NOT NULL,
     
     CONSTRAINT PK_Aluno PRIMARY KEY (ID),
     CONSTRAINT UK_Aluno UNIQUE(Tipo_Documento, Numero_Documento, Sigla_Pais),
@@ -112,35 +123,39 @@ CREATE TABLE Aluno (
         ON DELETE CASCADE,
     CONSTRAINT FK_Aluno_Equipe FOREIGN KEY (Nome_Equipe, Ano_Equipe)
         REFERENCES Equipe(Nome, Ano)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT CK_Aluno_Contato CHECK (Contato IS NULL OR Contato ~ '^\+?[0-9]{8,15}$'),
+    CONSTRAINT CK_Resp_Contato CHECK (Contato_Responsavel IS NULL OR Contato_Responsavel ~ '^\+?[0-9]{8,15}$')
 );
 
 CREATE TABLE Partida (
-    ID NUMBER NOT NULL,
-    Torneio NUMBER NOT NULL,
-    Fase VARCHAR2(13) NOT NULL,
-    Regiao VARCHAR2(255) NOT NULL,
-    Local_Partida VARCHAR2(255) NOT NULL,
-    DT_Inicio DATE NOT NULL,
-    DT_Fim DATE NOT NULL,
-    AlunoMVP NUMBER,
+    ID INTEGER NOT NULL,
+    Torneio INTEGER NOT NULL,
+    Fase VARCHAR(13) NOT NULL,
+    Regiao VARCHAR(255) NOT NULL,
+    Local_Partida VARCHAR(255) NOT NULL,
+    DataHora_Inicio DATE NOT NULL,
+    DataHora_Fim DATE NOT NULL,
+    AlunoMVP INTEGER,
     
     CONSTRAINT PK_Partida PRIMARY KEY (ID),
-    CONSTRAINT UK_Partida UNIQUE(Torneio, Fase, Regiao, Local_Partida),
+    -- VERIFICAR: CONSTRAINT UK_Partida UNIQUE(Torneio, Fase, Regiao, Local_Partida),
+    CONSTRAINT UK_Partida UNIQUE(Torneio, Local_Partida),
     CONSTRAINT FK_Partida_Torneio FOREIGN KEY (Torneio)
         REFERENCES Torneio(Ano)
         ON DELETE CASCADE,
     CONSTRAINT FK_Partida_MVP FOREIGN KEY (AlunoMVP)
         REFERENCES Aluno(ID)
         ON DELETE SET NULL,
-    CONSTRAINT CK_Fase CHECK(UPPER(Fase) IN ('REGIONAL', 'NACIONAL', 'CONTINENTAL', 'INTERNACIONAL'))
+    CONSTRAINT CK_Fase CHECK(UPPER(Fase) IN ('REGIONAL', 'NACIONAL', 'CONTINENTAL', 'INTERNACIONAL')),
+    CONSTRAINT CK_Datas_Partida CHECK (DT_Fim >= DT_Inicio) 
 );
 
 CREATE TABLE Equipe_Participa_Partida (
-    Nome_Equipe VARCHAR2(255) NOT NULL,
-    Ano_Equipe NUMBER NOT NULL,
-    Partida NUMBER NOT NULL,
-    Pontuacao NUMBER DEFAULT 0 NOT NULL,
+    Nome_Equipe VARCHAR(255) NOT NULL,
+    Ano_Equipe INTEGER NOT NULL,
+    Partida INTEGER NOT NULL,
+    Pontuacao NUMERIC DEFAULT 0 NOT NULL,
     
     CONSTRAINT PK_Equipe_Participa_Partida PRIMARY KEY (Nome_Equipe, Ano_Equipe, Partida),
     CONSTRAINT FK_EQUIPE_PARTICIPA FOREIGN KEY (Nome_Equipe, Ano_Equipe)
@@ -149,42 +164,49 @@ CREATE TABLE Equipe_Participa_Partida (
     CONSTRAINT FK_Participa_Partida FOREIGN KEY (Partida)
         REFERENCES Partida(ID)
         ON DELETE CASCADE
+    CONSTRAINT CK_Pontuacao_Minima CHECK (Pontuacao >= 0)
 );
 
 CREATE TABLE Ponto_de_Coleta (
-    ID NUMBER NOT NULL,
-    Partida NUMBER NOT NULL,
-    Latitude NUMBER(10, 6) NOT NULL,
-    Longitude NUMBER(10, 6) NOT NULL,
+    ID INTEGER NOT NULL,
+    Partida INTEGER NOT NULL,
+    Latitude NUMERIC(10, 6) NOT NULL,
+    Longitude NUMERIC(10, 6) NOT NULL,
     
     CONSTRAINT PK_Ponto_de_Coleta PRIMARY KEY (ID),
     CONSTRAINT UK_Ponto_de_Coleta UNIQUE(Partida, Latitude, Longitude),
     CONSTRAINT FK_Ponto_de_Coleta FOREIGN KEY (Partida)
         REFERENCES Partida(ID)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT CK_Latitude CHECK (Latitude BETWEEN -90 AND 90),
+    CONSTRAINT CK_Longitude CHECK (Longitude BETWEEN -180 AND 180) 
 );
 
 CREATE TABLE Coleta (
-    Aluno NUMBER NOT NULL,
-    Ponto_de_Coleta NUMBER NOT NULL,
-    Data_Hora DATE NOT NULL,
-    Lixo VARCHAR2(30) NOT NULL,
-    Peso NUMBER NOT NULL,
-    Pontuacao NUMBER NOT NULL,
+    Aluno INTEGER NOT NULL,
+    Ponto_de_Coleta INTEGER NOT NULL,
+    Data_Hora TIMESTAMP NOT NULL,
+    Lixo VARCHAR(30) NOT NULL,
+    Peso NUMERIC NOT NULL,
+    Pontuacao NUMERIC NOT NULL,
     
     CONSTRAINT PK_Coleta PRIMARY KEY (Aluno, Ponto_de_Coleta, Data_Hora),
+    CONSTRAINT FK_Coleta_Aluno FOREIGN KEY (Aluno)
+        REFERENCES Aluno(ID)
+        ON DELETE CASCADE,
     CONSTRAINT FK_Coleta_Ponto FOREIGN KEY (Ponto_de_Coleta)
         REFERENCES Ponto_de_Coleta(ID)
         ON DELETE CASCADE,
     CONSTRAINT FK_Coleta_Lixo FOREIGN KEY (Lixo)
         REFERENCES Lixo(Categoria)
-        ON DELETE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT CK_Coleta_Peso CHECK (Peso > 0)
 );
 
 CREATE TABLE Logistica_Transporte (
-    Ponto_de_Coleta NUMBER NOT NULL,
-    Transportadora NUMBER NOT NULL,
-    Centro_de_Reciclagem NUMBER NOT NULL,
+    Ponto_de_Coleta INTEGER NOT NULL,
+    Transportadora INTEGER NOT NULL,
+    Centro_de_Reciclagem INTEGER NOT NULL,
     
     CONSTRAINT PK_Logistica PRIMARY KEY (Ponto_de_Coleta, Transportadora, Centro_de_Reciclagem),
     CONSTRAINT FK_Logistica_Ponto FOREIGN KEY (Ponto_de_Coleta)
